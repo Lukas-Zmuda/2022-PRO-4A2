839d1f5b13aab2bd07bd87d2f76ab4d9d5493ad6
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-10-03 11:02:44 +0200

    komentare

--- MojPrvy/src/PrvyProgram.java
+++ MojPrvy/src/PrvyProgram.java
@@ -3,8 +3,24 @@ public class PrvyProgram {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Ahoj 4A");
+		//jednoriadkovy komentar
 		
+		/*
+		 * toto je 
+		 * viacriadkovy
+		 * komentar
+		 */
+		
+		/**
+		 * javadoc komentar
+		 */
+		System.out.println("Ahoj 4A");
+		System.out.println("Dnes je polooblacno");
+		
+		System.out.print("Jan");
+		System.out.println("Hrasko");
+		System.out.printf("Ahoj %s. Dnes je %d. oktobra.\n", "Fero", 3);
+		System.out.println("trsalsls");
 
 	}
 
