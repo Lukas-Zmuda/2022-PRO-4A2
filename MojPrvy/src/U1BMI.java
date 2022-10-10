780bb6fdd9f0034a9756e99db8bef8df6fe7952b
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-10-10 10:10:51 +0200

    ulohy

---
+++ MojPrvy/src/U1BMI.java
@@ -0,0 +1,17 @@
+
+public class U1BMI {
+
+	public static void main(String[] args) {
+		
+		int vyska = 186;
+		int hmotnost = 86;
+		double bmi = 0;
+		
+		//TODO
+		
+		System.out.println("Vase BMI: " + bmi);
+		
+
+	}
+
+}
---
+++ MojPrvy/src/U2Kvadraticka.java
@@ -0,0 +1,15 @@
+
+public class U2Kvadraticka {
+
+	public static void main(String[] args) {
+		
+		int a = 1, b = 2, c = 3;
+		
+		double diskriminant = 0;
+		
+		
+		System.out.println("D = " + diskriminant);
+
+	}
+
+}
