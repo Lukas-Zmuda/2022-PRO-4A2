6580d9e42b1fb6ae66f1555a1185b478e876e6b4
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2023-01-09 11:07:54 +0100

    Create U3KamenPapierNoznice.java

---
+++ MojPrvy/src/U3KamenPapierNoznice.java
@@ -0,0 +1,16 @@
+
+public class U3KamenPapierNoznice {
+
+	public static void main(String[] args) {
+		
+		//import potrebnych tried
+		//v troch kolach>
+		//uzivatel zada volbu (1-kamen, 2 - papier, 3 - noznice)
+		//pocitac vygeneruje volbu
+		//porovna sa volba uzivatela s volbou pc a nasledne sa vyhodnoti (vyhra, prehra, remiza)
+		//po troch kolach program vypise vitaza a skore
+		//koniec programu
+
+	}
+
+}
