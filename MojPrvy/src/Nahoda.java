e02f28dc2c2f4ca5f2b7279d5eb230661543da34
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2023-01-09 11:04:28 +0100

    Create Nahoda.java

---
+++ MojPrvy/src/Nahoda.java
@@ -0,0 +1,15 @@
+
+public class Nahoda {
+
+	public static void main(String[] args) {
+		
+		double nahodneCislo = Math.random();
+		System.out.println(nahodneCislo);
+		
+		int nahCeleCislo = (int) (Math.random() * 100) + 100;
+		
+		System.out.println(nahCeleCislo);
+
+	}
+
+}
