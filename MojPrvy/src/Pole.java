9ba6608ff4d0ad49e67ae57c73122fbf4ad2d581
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2023-01-23 11:28:00 +0100

    Create Pole.java

---
+++ MojPrvy/src/Pole.java
@@ -0,0 +1,42 @@
+
+public class Pole {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		//deklaracia pola
+		int[] cisla;
+		
+		//inicializacia pola
+		cisla = new int[7];
+		
+		cisla[2] = 4;
+		
+		System.out.println(cisla[2]);
+		
+		cisla[0] = 11;
+		cisla[1] = -3;
+		System.out.println("-------------");
+		for(int i = 0; i < 7; i++) {
+			System.out.print(cisla[i] + " ");
+		}
+		
+		
+		String[] mena = {"David", "Martin", "Robo", "Oliver", "Erik"};
+		
+		System.out.println("\nDruhy v zozname je " + mena[1]);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
